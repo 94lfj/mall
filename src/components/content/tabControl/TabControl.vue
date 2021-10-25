a5ef890a9c6dc74870bb5295bc7c84c0693dc682
94lfj <lfj17600586756@163.com>
2021-10-25 21:50:23 +0800

    第一天回家后学习代码

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="tabcontrol">
+    <div v-for="(item,index) in titles" :key="item" 
+        class="tab-control-item"
+        :class="{active: index === currenIndex}"
+        @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:"TabControl",
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      currenIndex:0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currenIndex = index;
+    }
+  }
+}
+</script>
+<style scoped>
+.tabcontrol{
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  height: 40px;
+  line-height: 40px;
+  background: #fff;
+}
+.tabcontrol .tab-control-item {
+  flex: 1;
+}
+.tab-control-item span{
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 3px solid var(--color-high-text);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,13 +4,53 @@
     <div slot="center">购物街</div>
   </nav-bar>
   <homeSwiper :banners="banners"/>
+  <recommend-view :recommends="recommends"/>
+  <feature></feature>
+  <tab-control class="tab-control" :titles="['流行','新款','精选']" />
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
 </div>
 </template>
 
 <script>
 
 import NavBar from "components/common/navbar/NavBar";
+import TabControl from 'components/content/tabControl/TabControl'
+
 import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from './childComps/RecommendView'
+import Feature from './childComps/Feature'
+
 import {getHomeMultidata} from "network/home";
 
 
@@ -18,7 +58,10 @@ export default {
   name: "Home",
   components: {
     NavBar,
-    HomeSwiper
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    Feature
   },
   data() {
     return {
@@ -38,8 +81,21 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
